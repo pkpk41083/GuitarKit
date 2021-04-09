b67dcfe0200f532360575fea24b4892480ebc84c
Andy <pkpk41083@gmail.com>
2021-04-09 14:20:36 +0800

    Initial Commit

---
+++ GuitarUtilities/GuitarUtilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarUtilitiesApp.swift
+//  GuitarUtilities
+//
+//  Created by Yukai Chang on 2021/4/9.
+//
+
+import SwiftUI
+
+@main
+struct GuitarUtilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
